b40389b9614d8ad752a5e54f3bc7bc71e28faa8a
Lamarjg <112987851+Lamarjg@users.noreply.github.com>
2022-09-25 18:28:23 -0400

    Add files via upload

---
+++ project.java
@@ -0,0 +1,35 @@
+package classProject1;
+
+import java.util.Scanner;
+
+public class project {
+
+	public static void main(String[] args) {
+	
+		Scanner myObj = new Scanner(System.in);
+		
+		int num1 = 150, num2 = 10;
+		
+		System.out.println("num1 + num2 = "+ (num1 + num2));
+		
+		System.out.println("num1 + num2 = "+ (num1 - num2));
+		
+		System.out.println("num1 + num2 = "+ (num1 * num2));
+		
+		System.out.println("num1 + num2 = "+ (num1 / num2));
+		
+		System.out.println("num1 + num2 = "+ (num1 % num2));
+//test
+
+
+
+				
+
+		
+		
+
+		
+	}
+
+}
+
